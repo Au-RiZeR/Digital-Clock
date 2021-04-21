a86f82f486b91890b4a6475099f23daff4c9cd1d
Ricky Gobby <Baxterrickygobby@gmail.com>
2021-04-22 07:53:40 +0800

    completed 7 segment numbers

--- scripts.js
+++ scripts.js
@@ -1,40 +1,175 @@
 $(document).ready(function () {
     var clockArea = $('main')
     var segments = 7
-    var seven = ['top','topLeft','topright','centre','bottomLeft','BottomRight','bottom']
-    
-for (var n = 1; n < 5; n++) {
-    if (segments == 7) {
-        sevenSegments()      
+    var seven = ['top', 'topLeft', 'topRight', 'centre', 'bottomLeft', 'bottomRight', 'bottom']
+    var time = 24
+
+    setInterval(() => {
+        for (let i = 1; i < 5; i++) {
+            resetter(i);
+        }
+        if (time == 24) {
+            var hour = moment().format('HH');
+        } else {
+            var hour = moment().format('hh');
+        }
+        var minute = moment().format('mm');
+        if (hour.length > 1) {
+            var first = hour.split('')[0];
+            var second = hour.split('')[1];
+            eval(`n${segments + first}(1)`);
+        } else {
+            var second = hour
+        }
+        var third = minute.split('')[0];
+        var forth = minute.split('')[1];
+        eval(`n${segments + second}(2)`);
+        eval(`n${segments + third}(3)`);
+        eval(`n${segments + forth}(4)`);
+        // flash();
+    }, 1000);
+
+    function resetter(placement) {
+        for (let i = 0; i < 7; i++) {
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            let newClasses = existingClasses.replace(' lit', '');
+            $(element).attr('class', newClasses)
+        }
     }
-}
 
-function unlitter(number) {
-    for (let i = 0; i < number.childNodes.length; i++) {
-        let div = $(number.childNodes[i])
-        let id = seven[i]
-        $(div).attr('class', 'unlit');
-        $(div).attr('id', n + id);
+
+
+
+
+    for (var n = 1; n < 5; n++) {
+        if (segments == 7) {
+            sevenSegments()
+        }
     }
-}
 
-function sevenSegments() {
-    let number = document.createElement('div')
-    let top = document.createElement('div')
-    let topLeft = document.createElement('div')
-    let topRight = document.createElement('div')
-    let centre = document.createElement('div')
-    let bottomLeft = document.createElement('div')
-    let bottomRight = document.createElement('div')
-    let bottom = document.createElement('div')
-    $(top,topLeft, topRight, centre, bottomLeft, bottomRight, bottom).attr('class', 'unlit');
-    $(number).append(top, topLeft, topRight, centre, bottomLeft, bottomRight, bottom);
-    $(clockArea).append(number);
-    unlitter(number)
-}
+    function unlitter(number) {
+        for (let i = 0; i < number.childNodes.length; i++) {
+            let div = $(number.childNodes[i])
+            let id = seven[i]
+            $(div).attr('class', `unlit ${id}`);
+        }
+    }
+
+    function sevenSegments() {
+        let number = document.createElement('div')
+        $(number).attr('id', 'number'+n);
+        console.log($(number).attr('id'))
+        $(number).attr('class', 'number');
+        let top = document.createElement('div')
+        let topLeft = document.createElement('div')
+        let topRight = document.createElement('div')
+        let centre = document.createElement('div')
+        let bottomLeft = document.createElement('div')
+        let bottomRight = document.createElement('div')
+        let bottom = document.createElement('div')
+        $(top, topLeft, topRight, centre, bottomLeft, bottomRight, bottom).attr('class', 'unlit');
+        $(number).append(top, topLeft, topRight, centre, bottomLeft, bottomRight, bottom);
+        $(clockArea).append(number);
+        unlitter(number)
+    }
 
 
 
+    function n70(placement) {
+        for (let i = 0; i < 7; i++) {
+            if (i == 3) {
+                i++
+            }
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n71(placement) {
+        for (let i = 2; i < 7; i += 3) {
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n72(placement) {
+        for (let i = 0; i < 7; i++) {
+            if (i == 1 || i == 5) {
+                i++
+            }
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n73(placement) {
+        for (let i = 0; i < 7; i++) {
+            if (i == 1 || i == 4) {
+                i++
+            }
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n74(placement) {
+        for (let i = 1; i < 7; i++) {
+            if (i == 4 || i == 6) {
+                i++
+            }
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n75(placement) {
+        for (let i = 0; i < 7; i++) {
+            if (i == 2 || i == 4) {
+                i++
+            }
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n76(placement) {
+        for (let i = 0; i < 7; i++) {
+            if (i == 2) {
+                i++
+            }
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n77(placement) {
+        for (let i = 0; i < 7; i += 2) {
+            if (i == 4) {
+                i++
+            }
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n78(placement) {
+        for (let i = 0; i < 7; i++) {
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
+    function n79(placement) {
+        for (let i = 0; i < 7; i++) {
+            if (i == 4) {
+                i++
+            }
+            let element = $(`#number${placement} > div`)[i];
+            let existingClasses = $(element).attr('class');
+            $(element).attr('class', existingClasses + ' lit');
+        }
+    }
 
 
 
